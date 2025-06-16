829736f18eb4541c3490fae437b10b6977a9766f
Jonathas Nicacio Silva <jonathas.github@gmail.com>
2025-06-16 18:57:29 +0000

    teste 2

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -8,6 +8,7 @@
 #define coluna 10
 
 int main() {
+    
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
